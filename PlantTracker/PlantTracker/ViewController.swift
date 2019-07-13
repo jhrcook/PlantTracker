d42438499c34885cb2eff08f5e1ac14c6c67f345
Josh Cook <joshuacook0023@gmail.com>
2019-07-12 21:37:09 -0700

    initial commit of new Xcode project

---
+++ PlantTracker/PlantTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlantTracker
+//
+//  Created by Joshua on 7/12/19.
+//  Copyright © 2019 JHC Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
