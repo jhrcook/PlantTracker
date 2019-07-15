0f83e3d9d6885a7cbe210288354ab34c8428d794
Josh Cook <joshuacook0023@gmail.com>
2019-07-14 22:13:14 -0700

    feat: layout of library detail view image set up

--- PlantTracker/PlantTracker/LibraryDetailViewController.swift
+++ PlantTracker/PlantTracker/LibraryDetailViewController.swift
@@ -11,31 +11,31 @@ import UIKit
 class LibraryDetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var mainScrollView: UIScrollView!
-    @IBOutlet var testLabel: UILabel!
+    
+    @IBOutlet var headerView: UIView!
     @IBOutlet var headerImageView: UIImageView!
     
-    var headerHeight = CGFloat(50.0)
+    @IBOutlet var testLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mainScrollView.delegate = self
-        mainScrollView.contentSize.width = view.frame.width
-        mainScrollView.contentSize.height = 2000
-    
-        testLabel.text = "Scroll view test"
+        testLabel.text = "test label"
+
+        headerImageView.image = UIImage(named: "cactus")
+        headerImageView.contentMode = .scaleAspectFit
+        headerImageView.clipsToBounds = true
+        headerImageView.backgroundColor = .blue
         
-        headerImageView = UIImageView(image: UIImage(named: "cactus"))
-        headerImageView.contentMode = .scaleAspectFill
-        headerImageView.heightAnchor.constraint(equalToConstant: headerHeight).isActive = true
-        headerImageView.backgroundColor = .green
+    
+        mainScrollView.delegate = self
+        mainScrollView.contentSize = CGSize(width: view.frame.width, height: 3000.0)
+        mainScrollView.backgroundColor = .green
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offset = scrollView.contentOffset
-        testLabel.text = "\(offset.x), \(offset.y)"
-        
-        
+        print("scrollView.contentOffset: \(scrollView.contentOffset)")
+        testLabel.text = "x: \(scrollView.contentOffset.x), y: \(scrollView.contentOffset.y)"
         
     }
     
