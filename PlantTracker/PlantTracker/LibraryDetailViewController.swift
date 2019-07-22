babaabf430bf9ed1b060dd18dfc6938efcc1186e
Josh Cook <joshuacook0023@gmail.com>
2019-07-21 17:21:18 -0700

    feat: sticky header in library detail view

--- PlantTracker/PlantTracker/LibraryDetailViewController.swift
+++ PlantTracker/PlantTracker/LibraryDetailViewController.swift
@@ -7,35 +7,71 @@
 //
 
 import UIKit
+import SnapKit
 
 class LibraryDetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var mainScrollView: UIScrollView!
-    
     @IBOutlet var headerView: UIView!
     @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
     
-    @IBOutlet var testLabel: UILabel!
+    var startingYOffset: CGFloat? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        testLabel.text = "test label"
-
-        headerImageView.image = UIImage(named: "cactus")
-        headerImageView.contentMode = .scaleAspectFit
-        headerImageView.clipsToBounds = true
-        headerImageView.backgroundColor = .blue
-        
-    
-        mainScrollView.delegate = self
-        mainScrollView.contentSize = CGSize(width: view.frame.width, height: 3000.0)
-        mainScrollView.backgroundColor = .green
+        setupDetailView()
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("scrollView.contentOffset: \(scrollView.contentOffset)")
-        testLabel.text = "x: \(scrollView.contentOffset.x), y: \(scrollView.contentOffset.y)"
+        if startingYOffset == nil { startingYOffset = scrollView.contentOffset.y }
+        updateHeaderImage(offset: scrollView.contentOffset)
+    }
+    
+    func updateHeaderImage(offset: CGPoint) {
+        let scrollViewYDiff = startingYOffset! - offset.y
+        
+        headerView.frame = CGRect(x: 0, y: offset.y, width: headerView.frame.width, height: headerView.frame.height)
+        
+        if scrollViewYDiff == 0 {
+            return
+        } else if scrollViewYDiff > 0 {
+            // scrolling up
+            
+            // zoom in at most 1.3x
+            let scaleFactor = min(scrollViewYDiff / 200.0 + 1.0, 1.3)
+            headerImageView.transform = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
+            
+            print("SCROLLING UP: scroll view difference: \(scrollViewYDiff) -- scale factor: \(scaleFactor)")
+        } else  if scrollViewYDiff < 0 {
+            // scrolling down
+        }
+    }
+
+    
+    func setupDetailView() {
+        
+        // main scroll view
+        mainScrollView.delegate = self
+        mainScrollView.contentSize = CGSize(width: view.frame.width, height: 3000.0)
+        mainScrollView.backgroundColor = .green
+        mainScrollView.snp.makeConstraints { (make) in make.edges.equalTo(view) }
+        
+        // header view
+        headerView.snp.makeConstraints { (make) in
+            make.top.equalTo(mainScrollView.snp.top).priority(.medium)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+            make.height.equalTo(400)
+        }
+        
+        // header image
+        headerImageView.image = UIImage(named: "cactus")
+        headerImageView.contentMode = .scaleAspectFill
+        headerImageView.clipsToBounds = true
+        headerImageView.snp.makeConstraints { (make) in make.edges.equalTo(headerView) }
+        
+        // segmented control setup
         
     }
     
