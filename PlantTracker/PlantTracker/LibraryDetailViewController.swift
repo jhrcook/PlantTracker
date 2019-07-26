70a20ca0b7a3651051af0ca15bfb2b65c1aa7707
Josh Cook <joshuacook0023@gmail.com>
2019-07-26 08:04:36 -0400

    feat: sticky header with parallax and blurring
    Using `remakeConstraints`, the header view has a parallax effect.
    There is also a blur effect when scrolling down.

--- PlantTracker/PlantTracker/LibraryDetailViewController.swift
+++ PlantTracker/PlantTracker/LibraryDetailViewController.swift
@@ -18,33 +18,56 @@ class LibraryDetailViewController: UIViewController, UIScrollViewDelegate {
     
     var startingYOffset: CGFloat? = nil
     
+    // height of header image
+    let headerImageHeight = 350
+    let minHeaderImageHeight = 100
+    
+    var blurEffectView: UIVisualEffectView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDetailView()
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // initialize starting Y offset
         if startingYOffset == nil { startingYOffset = scrollView.contentOffset.y }
+        
+        // update header
         updateHeaderImage(offset: scrollView.contentOffset)
     }
     
     func updateHeaderImage(offset: CGPoint) {
         let scrollViewYDiff = startingYOffset! - offset.y
         
-        headerView.frame = CGRect(x: 0, y: offset.y, width: headerView.frame.width, height: headerView.frame.height)
+        // sticky header
+        let newHeight = max(CGFloat(headerImageHeight) + scrollViewYDiff, CGFloat(minHeaderImageHeight))
+        headerView.snp.remakeConstraints { (make) in
+            make.top.equalTo(view).offset(abs(startingYOffset!))
+            make.height.equalTo(newHeight)
+            make.trailing.equalTo(view)
+            make.leading.equalTo(view)
+        }
         
         if scrollViewYDiff == 0 {
             return
         } else if scrollViewYDiff > 0 {
             // scrolling up
+            print("SCROLLING UP: scroll view difference: \(scrollViewYDiff)")
             
-            // zoom in at most 1.3x
-            let scaleFactor = min(scrollViewYDiff / 200.0 + 1.0, 1.3)
-            headerImageView.transform = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
             
-            print("SCROLLING UP: scroll view difference: \(scrollViewYDiff) -- scale factor: \(scaleFactor)")
+            
         } else  if scrollViewYDiff < 0 {
             // scrolling down
+            print("SCROLLING DOWN: scroll view difference: \(scrollViewYDiff)")
+            
+            let frameHeight = CGFloat(headerView.frame.height)
+            let maxHeight = CGFloat(headerImageHeight)
+            let minHeight = CGFloat(minHeaderImageHeight)
+            let blurAlpha = (frameHeight - maxHeight) / (maxHeight - minHeight) * (0.0 - 0.7) + 0.0
+            print("blur alpha: \(blurAlpha)")
+            blurEffectView.alpha = blurAlpha
+            
         }
     }
 
@@ -58,21 +81,48 @@ class LibraryDetailViewController: UIViewController, UIScrollViewDelegate {
         mainScrollView.snp.makeConstraints { (make) in make.edges.equalTo(view) }
         
         // header view
+        headerView.layer.borderColor = UIColor.orange.cgColor
+        headerView.layer.borderWidth = 3.0
         headerView.snp.makeConstraints { (make) in
-            make.top.equalTo(mainScrollView.snp.top).priority(.medium)
+            make.top.equalTo(mainScrollView)
             make.leading.equalTo(view)
             make.trailing.equalTo(view)
-            make.height.equalTo(400)
+            make.height.equalTo(headerImageHeight)
         }
         
         // header image
+        headerImageView.layer.borderColor = UIColor.purple.cgColor
+        headerImageView.layer.borderWidth = 3.0
         headerImageView.image = UIImage(named: "cactus")
         headerImageView.contentMode = .scaleAspectFill
         headerImageView.clipsToBounds = true
         headerImageView.snp.makeConstraints { (make) in make.edges.equalTo(headerView) }
         
         // segmented control setup
+        segmentedControl.setTitle("Information", forSegmentAt: 0)
+        segmentedControl.setTitle("Notes", forSegmentAt: 1)
+        segmentedControl.setTitle("Links", forSegmentAt: 2)
+        segmentedControl.setEnabled(true, forSegmentAt: 0)
+        segmentedControl.addTarget(self, action: #selector(updateSegmentedControl), for: .valueChanged)
+        segmentedControl.snp.makeConstraints { (make) in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+//            make.height.equalTo(UIBarMetrics.default)
+        }
         
+        // blur effect on header
+        blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+        blurEffectView.alpha = 0.0
+        headerView.addSubview(blurEffectView)
+        blurEffectView.snp.makeConstraints { (make) in
+            make.edges.equalTo(headerView)
+        }
+        
+    }
+    
+    @objc func updateSegmentedControl() {
+        print("segment selection index: \(segmentedControl.selectedSegmentIndex)")
     }
     
 }
