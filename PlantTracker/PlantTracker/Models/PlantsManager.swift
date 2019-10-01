2d7ed21c2331c2220e19515dbe3b2ee2f2538a62
Josh Cook <joshuacook0023@gmail.com>
2019-10-01 10:39:35 -0400

    doc: doc for PlantsManager

--- PlantTracker/PlantTracker/Models/PlantsManager.swift
+++ PlantTracker/PlantTracker/Models/PlantsManager.swift
@@ -9,14 +9,21 @@
 import UIKit
 import os
 
+/**
+ A manager of the fundamental plant objects for the app. It loads, saves, and adds  plants to the array `plants: [Plant]`.
+ This manager should be created at start-up and passed to various view controllers.
+ */
 class PlantsManager {
     
+    /// The central array that holds all of the `Plant` objects for the user.
     var plants = [Plant]()
     
+    /// Loads the plants into `plants: [Plant]`
     init() {
         loadPlants()
     }
     
+    /// Loads the plants from disk. It is already called during initialization.
     func loadPlants() {
         let defaults = UserDefaults.standard
         if let savedPlants = defaults.object(forKey: "plants") as? Data{
@@ -30,7 +37,7 @@ class PlantsManager {
         }
     }
     
-    
+    /// Save the plants to disk.
     func savePlants() {
         let jsonEncoder = JSONEncoder()
         if let savedData = try? jsonEncoder.encode(plants) {
@@ -42,7 +49,7 @@ class PlantsManager {
         }
     }
     
-    
+    /// Make a new plant with `nil` for scientific and common names and append to `plants: [Plant]`.
     func newPlant() {
         os_log("Making new Plant.", log: Log.plantsManager, type: .default)
         plants.append(Plant(scientificName: nil, commonName: nil))
@@ -53,7 +60,10 @@ class PlantsManager {
 
 
 // MARK: Testing
+
 extension PlantsManager {
+    
+    /// Makes a test array of plants for use during development.
     func makeTestPlantsArray() {
         os_log("Loading test plants.", log: Log.plantLibraryTableVC, type: .info)
         plants = [
